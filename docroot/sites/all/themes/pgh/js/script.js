a95522745304600da09ac1dd943dd43e939c2462
Frank Yonnetti <fyonnetti@gmail.com>
2013-11-19 15:58:34 -0500

    Only run menu generation and other theme jQuery once.

--- docroot/sites/all/themes/pgh/js/script.js
+++ docroot/sites/all/themes/pgh/js/script.js
@@ -13,99 +13,95 @@
 (function ($, Drupal, window, document, undefined) {
   'use strict';
 
-  // To understand behaviors, see https://drupal.org/node/756722#behaviors
-  Drupal.behaviors.my_custom_behavior = {
-    attach: function(context, settings) {
+  $(document).ready(function () {
+      // Application menu.
+      // ======================================================================
 
+      var app_menu_id = '#block-pgh-application-application-category-menu';
 
+      // Check is app menu block exist.
+      if ($(app_menu_id).length) {
 
-        // Show / hide business units on the dashboard.
-      	// ======================================================================
+        // Wrap application drop-down in active section UL parent for theming.
+        if ($(app_menu_id + ' ul.category-menu li a').hasClass('active')) {
+          $(app_menu_id + ' ul.category-menu li a.active').clone().prependTo(app_menu_id + ' .item-list').wrap('<ul class="selected-item"><li />');
+          $(app_menu_id + ' ul.category-menu').appendTo(app_menu_id + ' .item-list ul.selected-item li');
 
-        // Add open height state to each block.
-      	$('.business-unit-block').each(function() {
-      	  var $height_opened = $(this).height();
-          $(this).css('height', $height_opened);
-        });
-
-        // Set clickable toggle arrow.
-      	$('.business-unit-block').addClass('bu-closed')
-      	                         .prepend('<a href="#" class="toggle"><span>Toggle</span></a>');
-
-      	// Toggle show/hide class.
-      	$('.business-unit-block a.toggle').click(function() {
-          if ($(this).parent().hasClass('bu-closed')) {
-            $(this).parent().removeClass('bu-closed').addClass('bu-opened');
-          } else {
-            $(this).parent().removeClass('bu-opened').addClass('bu-closed');
+          // Add back and next arrows.
+          var $app_menu_arrows = $('#block-pgh-application-application-category-menu ul.category-menu li a.active').parent('li');
+          if ($app_menu_arrows.prev('li')) {
+            $app_menu_arrows.prev().children('a').clone().prependTo(app_menu_id + ' .item-list').wrap('<span class="prev" />');
           }
-          return false;
-        });
-
-
-
-        // Application menu.
-        // ======================================================================
-
-        var app_menu_id = '#block-pgh-application-application-category-menu';
-
-        // Check is app menu block exist.
-        if ($(app_menu_id).length) {
-
-          // Wrap application drop-down in active section UL parent for theming.
-          if ($(app_menu_id + ' ul.category-menu li a').hasClass('active')) {
-            $(app_menu_id + ' ul.category-menu li a.active').clone().prependTo(app_menu_id + ' .item-list').wrap('<ul class="selected-item"><li />');
-            $(app_menu_id + ' ul.category-menu').appendTo(app_menu_id + ' .item-list ul.selected-item li');
-
-            // Add back and next arrows.
-            var $app_menu_arrows = $('#block-pgh-application-application-category-menu ul.category-menu li a.active').parent('li');
-            if ($app_menu_arrows.prev('li')) {
-              $app_menu_arrows.prev().children('a').clone().prependTo(app_menu_id + ' .item-list').wrap('<span class="prev" />');
-            }
-            if ($app_menu_arrows.next('li')) {
-              $app_menu_arrows.next().children('a').clone().prependTo(app_menu_id + ' .item-list').wrap('<span class="next" />');
-            }
-
-            // Add the active category class to h1 by first grabbing the classes needed.
-            var active_classes = $('#block-pgh-application-application-category-menu ul.category-menu li a.active').attr('class');
-
-            // Second, apply classes to h1. Remove unnecessary class and put back the original classes.
-            $('h1#page-title').attr('class', active_classes)
-                              .removeClass('category-link active')
-                              .addClass('page__title title')
-                              .prepend('<span />'); // Add an empty SPAN to put the category icon into.
-
-          } else {
-            $(app_menu_id + ' ul.category-menu').wrap('<ul class="selected-item"><li />').parent().prepend('<a href="#">Select a section</a>');
-            $(app_menu_id + ' ul.category-menu li.first a').clone()
-                                                         .prependTo(app_menu_id + ' .item-list')
-                                                         .wrap('<span class="next" />');
-            $('h1#page-title').addClass('no-category');
+          if ($app_menu_arrows.next('li')) {
+            $app_menu_arrows.next().children('a').clone().prependTo(app_menu_id + ' .item-list').wrap('<span class="next" />');
           }
 
+          // Add the active category class to h1 by first grabbing the classes needed.
+          var active_classes = $('#block-pgh-application-application-category-menu ul.category-menu li a.active').attr('class');
+
+          // Second, apply classes to h1. Remove unnecessary class and put back the original classes.
+          $('h1#page-title').attr('class', active_classes)
+                            .removeClass('category-link active')
+                            .addClass('page__title title')
+                            .prepend('<span />'); // Add an empty SPAN to put the category icon into.
+
+        } else {
+          $(app_menu_id + ' ul.category-menu').wrap('<ul class="selected-item"><li />').parent().prepend('<a href="#">Select a section</a>');
+          $(app_menu_id + ' ul.category-menu li.first a').clone()
+                                                       .prependTo(app_menu_id + ' .item-list')
+                                                       .wrap('<span class="next" />');
+          $('h1#page-title').addClass('no-category');
         }
 
-        // App menu - hover intent
-        $(app_menu_id + ' ul.selected-item > li').hoverIntent({
-          sensitivity: 7, // number = sensitivity threshold (must be 1 or higher)
-          interval: 100, // number = milliseconds for onMouseOver polling interval
-          timeout: 140, // number = milliseconds delay before onMouseOut
-          over: showNav,  // function = onMouseOver callback (required)
-          out: hideNav    // function = onMouseOut callback (required)
-        });
-        function showNav(){$(this).addClass('hihover');}
-        function hideNav(){$(this).removeClass('hihover');}
+      }
 
 
+      // App menu - hover intent
+      $(app_menu_id + ' ul.selected-item > li').hoverIntent({
+        sensitivity: 7, // number = sensitivity threshold (must be 1 or higher)
+        interval: 100, // number = milliseconds for onMouseOver polling interval
+        timeout: 140, // number = milliseconds delay before onMouseOut
+        over: showNav,  // function = onMouseOver callback (required)
+        out: hideNav    // function = onMouseOut callback (required)
+      });
+
+      function showNav() {
+        $(this).addClass('hihover');
+      }
+
+      function hideNav() {
+        $(this).removeClass('hihover');
+      }
 
 
-        // Replace the fieldset legend with an H3 so it's easier to style
-      	// ======================================================================
-      	$('legend').replaceWith( '<h3 class="legend">' + $('legend').html() + '</h3>' );
+      // Show / hide business units on the dashboard.
+    	// ======================================================================
+
+      // Add open height state to each block.
+    	$('.business-unit-block').each(function() {
+    	  var $height_opened = $(this).height();
+        $(this).css('height', $height_opened);
+      });
+
+      // Set clickable toggle arrow.
+    	$('.business-unit-block').addClass('bu-closed')
+    	                         .prepend('<a href="#" class="toggle"><span>Toggle</span></a>');
+
+    	// Toggle show/hide class.
+    	$('.business-unit-block a.toggle').click(function() {
+        if ($(this).parent().hasClass('bu-closed')) {
+          $(this).parent().removeClass('bu-closed').addClass('bu-opened');
+        } else {
+          $(this).parent().removeClass('bu-opened').addClass('bu-closed');
+        }
+        return false;
+      });
 
 
-    }
-  };
+      // Replace the fieldset legend with an H3 so it's easier to style
+    	// ======================================================================
+    	$('legend').replaceWith( '<h3 class="legend">' + $('legend').html() + '</h3>' );
 
+  });
 
 })(jQuery, Drupal, this, this.document);
