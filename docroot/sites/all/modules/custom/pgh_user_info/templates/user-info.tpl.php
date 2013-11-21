2685f28493b849d33d641bbe8615c1f86481a7b4
Frank Yonnetti <fyonnetti@gmail.com>
2013-11-21 12:39:54 -0500

    Styled updated dashboard block.

--- docroot/sites/all/modules/custom/pgh_user_info/templates/user-info.tpl.php
+++ docroot/sites/all/modules/custom/pgh_user_info/templates/user-info.tpl.php
@@ -9,34 +9,33 @@
 
 ?>
 
-<div id="loggedin-user-block">
-  <div class="user-picture-wrap"><div class="user-picture">
+<div class="user-picture-wrap"><div class="user-picture">
 
-  <?php if ($variables['user']->picture): ?>
-  <?php
-    print theme('image_style',
-        array(
-          'style_name' => 'thumbnail',
-          'path' => $user->picture->uri,
-          'attributes' => array('class' => 'avatar')
-        )
-      );
-  ?>
-  <?php endif; ?>
+<?php if ($variables['user']->picture): ?>
+<?php
+  print theme('image_style',
+      array(
+        'style_name' => 'thumbnail',
+        'path' => $user->picture->uri,
+        'attributes' => array('class' => 'avatar')
+      )
+    );
+?>
+<?php endif; ?>
 
-  </div></div>
+</div></div>
 
+<div class="users-name">
+  <?php print $variables['user']->name; ?>
+  <br />
+  <span class="users-employer">
+    <?php
+    $user_company = field_view_field('user', $variables['user'], 'field_company', array('label' => 'hidden'));
+    print drupal_render($user_company); ?>
+  </span>
   <div class="user-links">
     <span class="edit-user"><a href="/user/<?php print $variables['user']->uid; ?>/edit">Edit</a></span>
     <span class="logout-user"><a href="/user/logout">Log out</a></span>
-  </div>
-  <div class="users-name">
-    <?php print $variables['user']->name; ?>
-    <br />
-    <span class="users-employer">
-      <?php
-      $user_company = field_view_field('user', $variables['user'], 'field_company', array('label' => 'hidden'));
-      print drupal_render($user_company); ?>
-    </span>
+    <span class="dashboard-user"><a href="/dashboard">Dashboard</a></span>
   </div>
 </div>
