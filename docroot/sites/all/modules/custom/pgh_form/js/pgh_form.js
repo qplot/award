b0ce101612dba7f4d2cdcc70797858ed28b1ba47
Jay Roberts <jay@gloryfish.org>
2013-11-26 13:56:49 -0500

    Add form save status.

---
+++ docroot/sites/all/modules/custom/pgh_form/js/pgh_form.js
@@ -0,0 +1,65 @@
+(function($) {
+  'use strict';
+
+  $(document).ready(function() {
+    // Add AJAX save status message.
+    $('#application-menu-container').append('<div class="pgh-form-status">All changes saved</div>');
+
+    // Hide submit button
+    $('#pgh-application-form').find('input[type="submit"]').hide();
+  });
+
+  Drupal.pghApplicationForm = {
+    form_id: 'pgh-application-form',
+  };
+
+  Drupal.behaviors.pghApplicationForm = {
+    attach: function(context, settings) {
+      // We extend Drupal.ajax objects for all AJAX elements in our form
+      for (var ajax_el in settings.ajax) {
+        if (typeof Drupal.ajax[ajax_el] != 'undefined' && Drupal.ajax[ajax_el].element.form) {
+          if (Drupal.ajax[ajax_el].element.form.id === Drupal.pghApplicationForm.form_id) {
+            Drupal.ajax[ajax_el].beforeSend = Drupal.pghApplicationForm.beforeSend;
+            Drupal.ajax[ajax_el].success = Drupal.pghApplicationForm.success;
+            Drupal.ajax[ajax_el].error = Drupal.pghApplicationForm.error;
+          }
+        }
+      }
+    }
+  };
+
+  // Disable form
+  Drupal.pghApplicationForm.beforeSend = function (first, second) {
+    console.log("Before Submit");
+
+    $('.pgh-form-status').addClass('saving')
+                         .text('Saving changes...');
+
+    if (typeof Drupal.ajax.prototype.beforeSend === 'function') {
+      Drupal.ajax.prototype.beforeSend.call(this, first, second);
+    }
+  }
+
+  Drupal.pghApplicationForm.success = function (response, status) {
+    console.log("On Success");
+
+    $('.pgh-form-status').addClass('saving')
+                         .text('All changes saved');
+
+    if (typeof Drupal.ajax.prototype.success === 'function') {
+      Drupal.ajax.prototype.success.call(this, response, status);
+    }
+  }
+
+  Drupal.pghApplicationForm.error = function (response, uri) {
+    console.log("Error");
+
+    $('.pgh-form-status').removeClass('saving')
+                         .text('Problem saving changes');
+
+    if (typeof Drupal.ajax.prototype.error === 'function') {
+      Drupal.ajax.prototype.error.call(this, response, uri);
+    }
+  }
+
+})(jQuery);
