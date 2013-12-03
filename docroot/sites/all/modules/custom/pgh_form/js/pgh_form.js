1dddc801ed69bb511383d04c6a072cea629a1c87
Jay Roberts <jay@gloryfish.org>
2013-12-03 15:50:35 -0500

    Fix indentation.

--- docroot/sites/all/modules/custom/pgh_form/js/pgh_form.js
+++ docroot/sites/all/modules/custom/pgh_form/js/pgh_form.js
@@ -64,35 +64,33 @@
     }
   };
 
-// Array of letters for dependent question labeling.
-var letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  // Array of letters for dependent question labeling.
+  var letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-Drupal.behaviors.pghApplicationNumbering = {
-  attach: function (context, settings) {
-    // Remove any existing number markup.
-    $('.question-number').remove();
+  Drupal.behaviors.pghApplicationNumbering = {
+    attach: function (context, settings) {
+      // Remove any existing number markup.
+      $('.question-number').remove();
 
-    var $all_questions = $('.question');
-    var $top_level_questions = $('.question:not(.question-dependent,.question-type-html)');
+      var $all_questions = $('.question');
+      var $top_level_questions = $('.question:not(.question-dependent,.question-type-html)');
 
-    // Add an ordinal to the first form-item of each top-level question.
-    $top_level_questions.each(function(top_index, top_element) {
-      // We prepend the number divs to the form-items so that numbering for dependent questions is
-      // properly hidden by Drupal's form system. A question may contain multiple form-items so we
-      // only add a number to the first matched element.
+      // Add an ordinal to the first form-item of each top-level question.
+      $top_level_questions.each(function(top_index, top_element) {
+        // We prepend the number divs to the form-items so that numbering for dependent questions is
+        // properly hidden by Drupal's form system. A question may contain multiple form-items so we
+        // only add a number to the first matched element.
 
-      var top_number = top_index + 1;
-      $(top_element).find('.form-item:eq(0)').prepend('<div class="question-number">' + top_number.toString() + '.</div>');
+        var top_number = top_index + 1;
+        $(top_element).find('.form-item:eq(0)').prepend('<div class="question-number">' + top_number.toString() + '.</div>');
 
-      // Add an alphabetic ordinal to each question which is a dependent question of the current
-      // top-level question.
-      $all_questions.filter('[data-parent-id="' + $(top_element).attr('id') + '"]').each(function(sub_index, sub_element) {
-        $(sub_element).find('.form-item:eq(0)').prepend('<div class="question-number sub">' + top_number.toString() + '.' + letters[sub_index] + '</div>');
+        // Add an alphabetic ordinal to each question which is a dependent question of the current
+        // top-level question.
+        $all_questions.filter('[data-parent-id="' + $(top_element).attr('id') + '"]').each(function(sub_index, sub_element) {
+          $(sub_element).find('.form-item:eq(0)').prepend('<div class="question-number sub">' + top_number.toString() + '.' + letters[sub_index] + '</div>');
+        });
       });
-    });
-  }
-};
-
-
+    }
+  };
 
 })(jQuery);
