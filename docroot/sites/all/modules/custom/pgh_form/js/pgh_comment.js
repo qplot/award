dd7fe47389b99ee34805fae83a5fdc42a96b6297
Fang Jin <windmaomao@gmail.com>
2014-02-03 11:45:13 -0500

    add custom js handling to comment submission

---
+++ docroot/sites/all/modules/custom/pgh_form/js/pgh_comment.js
@@ -0,0 +1,20 @@
+//
+// pgh_comment.js
+//
+// Provides front-end functionality for comment submission.
+//
+// @author Fang Jin <fang@designhammer.com>
+//
+(function($) {
+  'use strict';
+
+  $(document).ready(function () {
+
+    $('.use-custom-ajax').click(function(event) {
+      alert('123');
+      event.preventDefault();
+    });
+
+  });
+
+})(jQuery);
