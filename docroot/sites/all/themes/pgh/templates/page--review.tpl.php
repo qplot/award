8f2440599df41a07978b9e4fbcff34374d58eb64
Fang Jin <windmaomao@gmail.com>
2014-03-13 10:34:01 -0400

    add more fields to application section of review ui

--- docroot/sites/all/themes/pgh/templates/page--review.tpl.php
+++ docroot/sites/all/themes/pgh/templates/page--review.tpl.php
@@ -6,7 +6,7 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728148
  */
-dsm($application);
+  dsm($app);
 ?>
 
 <div id="page">
@@ -76,6 +76,43 @@ dsm($application);
     <div id="workgroup-content" class="column">
       <p class="dashboard-label">Work Group</p>
       <h1 class="page__title title">Review</h1>
+
+
+      <div id="hospital-wrap"><div id="hospital-wrap-inner">
+
+        <div id="hospital-content-left" class="column">
+          <p><?php echo $app['apptype'] ?></p>
+          <h2><?php echo $app['institution'] ?></h2>
+          <p><?php echo $app['institution_name'] ?></p>
+          <h3>Primary Contact:</h3>
+          <p></p>
+          <p><?php echo $app['city'] . ' ' . $app['state'] . ', ' . $app['zipcode'] ?></p>
+          <p><span class="semi-bold">Staffed Beds:</span> <?php echo $app['beds'] ?></p>
+          <p><span class="semi-bold">ORs:</span> <?php echo $app['ors'] ?></p>
+          <p><span class="semi-bold">FE Liaison:</span></p>
+          <p><span class="semi-bold">Reviewer(s):</span></p>
+        </div>
+
+        <div id="hospital-content-mid" class="column">
+          <h2>Application Name</h2>
+          <p><span class="semi-bold">Number of Acute Care Hospitals in Systems:</span> <?php echo $app['cares'] ?></p>
+          <p><span class="semi-bold">Number of Hospitals winning PFC or above:</span></p>
+          <p><span class="semi-bold">Does System qualify:</span></p>
+          <p><span class="semi-bold">Does application meet the metrics thresholds for award applied for ?</span></p>
+        </div>
+
+        <div id="hospital-content-right" class="column">
+          <h2>Suggested Awards</h2>
+          <h2>Final Awards:</h2>
+        </div>
+
+        <div class="pull-right">
+          <span class="edit-content">
+            <a href="">Save</a>
+          </span>
+        </div>
+
+      </div></div>
     </div>
 
   </div><!-- /#workgroup -->
