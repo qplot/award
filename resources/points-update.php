ae5e12f6c21d52ee6e0027c697c93ee8b1d80572
Kosta Harlan <kostajh@gmail.com>
2014-03-21 12:40:21 -0400

    Task #14: Allow passing start-row option

--- resources/points-update.php
+++ resources/points-update.php
@@ -4,6 +4,8 @@
 /**
  * @file
  * Drush script for updating Questions.
+ *
+ * @author Kosta Harlan
  */
 
 /**
@@ -18,10 +20,12 @@
  *
  * Options:
  *
- * Pass the `--file={filepath}` option to load a file instead of
+ *`--file={filepath}` option to load a file instead of
  * /compiled/points.csv. Useful* for testing.
  *
- * Pass the `--app-id={app_id}` option to update a specific app ID for testing.
+ * `--app-id={app_id}` option to update a specific app ID for testing.
+ * 
+ * `--start-row={row}` option lets you (re)start update at a certain row. 
  */
 
 
@@ -44,6 +48,7 @@ if (!$file) {
   $file = PGH_MIGRATE_DATA_DIR . '/compiled/points.csv';
 }
 $app_id = drush_get_option('app-id');
+$start_row = drush_get_option('start-row');
 
 drush_log(dt('Loading file !file', array('!file' => $file)), 'ok');
 if (!file_exists($file)) {
@@ -60,16 +65,20 @@ foreach ($data as $index => $row) {
     // Ignore CSV headers.
     continue;
   }
+  // Allow (re)starting the migration at a particular row.
+  if ($start_row && $index < $start_row) {
+    continue;
+  }
   drush_log(dt('### Processing row !row of total !total ###', array('!row' => $index, '!total' => $total)), 'ok');
   if (empty($row[1])) {
-    drush_log(dt('Skipping row !row, it does not have an ID.', array('!row' => $index)), 'ok');
+    drush_log(dt('- Skipping row !row, it does not have an ID.', array('!row' => $index)), 'ok');
     $ignore[] = $index;
     continue;
   }
   $id = $row[1];
   $nid = pgh_api_find_nid($id, 'question');
   if (!$nid) {
-    drush_set_error(dt('Could not find node ID from id !id', array('!id' => $id)));
+    drush_set_error(dt('- Could not find node ID from id !id', array('!id' => $id)));
     $ignore[] = $index;
     continue;
   }
@@ -77,12 +86,12 @@ foreach ($data as $index => $row) {
   $score = explode('|', $row[10]);
   $kpi = explode('|', $row[11]);
   if (!$point || !$score || !$kpi) {
-    drush_set_error(dt('KPI, point or score are not set for row !row', array('!row' => $index)), 'ok');
+    drush_set_error(dt('- KPI, point or score are not set for row !row', array('!row' => $index)), 'ok');
     $error[] = $index;
     continue;
   }
   $node = node_load($nid);
-  drush_log(dt('Loaded node !title', array('!title' => $node->title)), 'success');
+  drush_log(dt('- Loaded node !title', array('!title' => $node->title)), 'success');
   $wrapper = entity_metadata_wrapper('node', $node);
   $wrapper->field_question_point->set($point);
   $wrapper->field_question_score->set($score);
@@ -108,6 +117,7 @@ foreach ($data as $index => $row) {
     TRUE
   );
   $saved_responses = 0;
+  drush_log(dt('  - Found !count response IDs.', array('!count' => count($rids))), 'ok');
   foreach ($rids as $rid) {
     $response = node_load($rid);
     if (!empty($response->field_response_updated['und'][0]['value'])) {
@@ -120,7 +130,7 @@ foreach ($data as $index => $row) {
       $success[] = $index;
     }
   }
-  drush_log(dt('Saved !count responses for question !qid.', array(
+  drush_log(dt('  - Saved !count responses for question !qid.', array(
     '!count' => $saved_responses,
     '!qid' => $qid)), 'success');
 }
