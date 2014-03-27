cf69f84ccb1587e21e13f56e56781a19d160e414
Fang Jin <windmaomao@gmail.com>
2014-03-27 13:14:28 -0400

    address review ui issues on acute cares and calculation buttons

--- docroot/sites/all/themes/pgh/templates/review.tpl.php
+++ docroot/sites/all/themes/pgh/templates/review.tpl.php
@@ -44,7 +44,7 @@
             <?php if (!empty($app['ors'])): ?>
               <span class="label">ORs:</span> <?php echo $app['ors'] ?></p>
             <?php endif; ?>
-          <p><span class="label">FE Liaison:</span></p>
+          <p><span class="label">FE Liaison: Not Implemented</span></p>
           <p><span class="label">Reviewer(s):</span> 
             <?php if (!empty($app['reviewers'])): ?>
               <?php echo $app['reviewers'] ?>
@@ -56,7 +56,9 @@
 
         <div id="hospital-content-mid" class="column">
           <h3>Application Name: <?php echo $app['apptype'] ?></h3>
-          <p><span class="label">Number of Acute Care Hospitals in Systems:</span> <?php echo $app['cares'] ?> 36</p>
+          <?php if (!empty($app['cares'])): ?>
+            <p><span class="label">Number of Acute Care Hospitals in Systems:</span> <?php echo $app['cares'] ?> </p>
+          <?php endif; ?>
           <?php echo $form['winning_pfcs'] ?>
           <?php echo $form['qualify'] ?>
           <?php echo $form['threshold_met'] ?>
